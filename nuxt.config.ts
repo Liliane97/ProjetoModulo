75a7232ffe1f50628d3617e9c46ba7647392dc52
liliane97 <lilianebasilio1997@gmail.com>
2023-03-07 12:25:13 -0300

    meu primeiro teste com modulo

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import ProjetoModule from '../modulosNuxt/projeto-module/src/module'
+export default defineNuxtConfig({
+    modules: [
+        ProjetoModule
+      ]
+})
